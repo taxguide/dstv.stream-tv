ee7d67375763e51d3713183d8bfb61e88e58a67c
taxguide <singhparash06@gmail.com>
2026-07-21 17:06:27 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'DStv.Stream/TV'
+author = 'DStv.Stream/TV'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "DStv.Stream/TV"
